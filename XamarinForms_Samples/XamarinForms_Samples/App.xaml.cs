f35e6a1c32762480de831420892511fc70a2c4e3
Dilip <dilip.geevarghese@ivedix.com>
2018-04-10 18:07:08 +0530

    Storing values in ApplicationProperties

--- XamarinForms_Samples/XamarinForms_Samples/App.xaml.cs
+++ XamarinForms_Samples/XamarinForms_Samples/App.xaml.cs
@@ -9,15 +9,49 @@ namespace XamarinForms_Samples
 {
 	public partial class App : Application
 	{
+        private const string TitleKey = "Name";
+        private const string NotificationEnabledKey = "NotificationEnabled";
 		public App ()
 		{
 			InitializeComponent();
 
             //MainPage = new XamarinForms_Samples.MainPage();
-            MainPage = new NavigationPage(new XamarinForms_Samples.ContactsList());
+            //MainPage = new NavigationPage(new XamarinForms_Samples.ContactsList());
+            //MainPage = new XamarinForms_Samples.TableViewSample();
+            MainPage = new XamarinForms_Samples.DataStorage.ApplicationProperties();
         }
 
-		protected override void OnStart ()
+        public string Title
+        {
+            get
+            {
+                if (Properties.ContainsKey(TitleKey))
+                    return Properties[TitleKey].ToString();
+                else
+                    return string.Empty;
+            }
+            set
+            {
+                Properties[TitleKey] = value;
+            }
+        }
+
+        public string NotificationEnabled
+        {
+            get
+            {
+                if (Properties.ContainsKey(NotificationEnabledKey))
+                    return Properties[NotificationEnabledKey].ToString();
+                else
+                    return string.Empty;
+            }
+            set
+            {
+                Properties[NotificationEnabledKey] = value;
+            }
+        }
+
+        protected override void OnStart ()
 		{
 			// Handle when your app starts
 		}
@@ -31,5 +65,7 @@ namespace XamarinForms_Samples
 		{
 			// Handle when your app resumes
 		}
+
+
 	}
 }
